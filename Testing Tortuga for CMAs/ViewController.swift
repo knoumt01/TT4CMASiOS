9147a995c4055044a3eb6e4aa808f5a4788247e1
Megan Hunter <Megan@megan-hunters-macbook-pro-5.local>
2015-04-09 12:38:44 -0400

    first commit

--- Testing Tortuga for CMAs/ViewController.swift
+++ Testing Tortuga for CMAs/ViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func buttonStartPressed(sender:UIButton) {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func buttonNotesPressed(sender:UIButton) {
+        
     }
-
-
+    
+    @IBAction func buttonExitPressed(sender:UIButton) {
+        
+    }
 }
 
